5d5a50ce5c2746d5feb300a2ab689f9119d0a42c
YinChengChen <YinChengChen>
2023-11-06 11:04:32 +0800

    version 4.1

--- src/components/CardInfo/CardIondelay.tsx
+++ src/components/CardInfo/CardIondelay.tsx
@@ -1,9 +1,17 @@
 import { useEffect, useState } from "react";
-import { Card, CardContent, Typography, Grid } from "@mui/material";
+import { Box, Card, CardContent, Typography, Grid, FormControl, Select, InputLabel, MenuItem, SelectChangeEvent } from "@mui/material";
 import LeafletMap from "../LeaflatMap/LeafletMap";
 import MapComponent from "../LeaflatMap/MapComponent";
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+
 export default function CardIondelay() {
-    const cardTitle = "電離層延遲量"
+    const cardTitle = "電離層延遲量";
+    const [selectItem, setSelectItem] = useState("");
+    const handleChange = (event: SelectChangeEvent) => {
+
+    };
     return (
         <Card>
             <CardContent>
@@ -16,9 +24,30 @@ export default function CardIondelay() {
                         <MapComponent/>
                     </Grid>
                     <Grid item xs={6}>
+                        <Box sx={{ pt: 2}}>
+                        <LocalizationProvider dateAdapter={AdapterDayjs}>
+                      <DatePicker />
+                    </LocalizationProvider>
+                        <FormControl fullWidth>
+                        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+                        <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={selectItem}
+                        label="Age"
+                        onChange={handleChange}
+                        >
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                        </Select>
+                        </FormControl>
                         <Typography>
                             Lorem ipsum dolor sit amet.
                         </Typography>
+
+                        </Box>
+                    
                     </Grid>
                 </Grid>
                 
